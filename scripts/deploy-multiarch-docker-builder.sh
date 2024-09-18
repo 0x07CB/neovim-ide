9217630380b61a19bd1447534035de4b391fed24
rick <83157348+0x07CB@users.noreply.github.com>
2024-09-18 19:05:13 +0200

    add linode stack-script

--- scripts/deploy-multiarch-docker-builder.sh
+++ scripts/deploy-multiarch-docker-builder.sh
@@ -1,10 +1,35 @@
 #!/bin/bash
-#
+
+# <UDF name="USERNAME" label="Nom d'utilisateur" />
+# <UDF name="PASSWORD" label="Mot de passe" />
+# <UDF name="SSH_KEY" label="Clé SSH publique" />
+
+# Création de l'utilisateur
+useradd -m -s /bin/bash "$USERNAME"
+
+# Définition du mot de passe
+echo "$USERNAME:$PASSWORD" | chpasswd
+
+# Ajout de l'utilisateur au groupe sudo
+usermod -aG sudo "$USERNAME"
+
+# Configuration de la clé SSH
+mkdir -p /home/$USERNAME/.ssh
+echo "$SSH_KEY" > /home/$USERNAME/.ssh/authorized_keys
+chmod 700 /home/$USERNAME/.ssh
+chmod 600 /home/$USERNAME/.ssh/authorized_keys
+chown -R $USERNAME:$USERNAME /home/$USERNAME/.ssh
+
+# Limitation des privilèges sudo
+echo "$USERNAME ALL=(ALL) NOPASSWD: /usr/bin/docker, /usr/sbin/service docker start, /usr/sbin/service docker stop, /usr/sbin/service docker restart" > /etc/sudoers.d/$USERNAME
+chmod 0440 /etc/sudoers.d/$USERNAME
+
+
 # ##########################################################
 # Script to setup an SSH Server with specific configurations on Debian 12
 #
 # Update system packages
-# Mise à jour complète du système
+# Mise à jour des paquets système
 apt-get update
 apt-get upgrade -y
 
@@ -29,7 +54,7 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   tee /etc/apt/sources.list.d/docker.list > /dev/null
-apt-get updatep
+apt-get update
 #
 apt-get install -y \
     docker-ce \
@@ -41,5 +66,8 @@ apt-get install -y \
 systemctl enable docker --now
 # ##########################################################
 # Add user to docker group
-usermod -aG docker $USER
+usermod -aG docker $USERNAME
 # ##########################################################
+
+
+
